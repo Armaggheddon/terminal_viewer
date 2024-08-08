41f819da767f87b843d494020e0d0b7235a68647
Armaggheddon <47779194+Armaggheddon@users.noreply.github.com>
2024-08-08 20:18:23 +0200

    Changed video backend to pyav instead of opencv for a broader compatibility and better seek support

--- terminal_viewer/media_handler/video_digest.py
+++ terminal_viewer/media_handler/video_digest.py
@@ -1,4 +1,8 @@
-import cv2
+import av
+import av.error
+import av.video
+import av.video.format
+import av.video.reformatter
 import numpy as np
 
 from .types import EndMediaError, MediaCommand, MediaDigest
@@ -9,14 +13,22 @@ class VideoDigest(MediaDigest):
     """
     def __init__(self, media_path: str) -> None:
         self.media_path = media_path
+        
+        self.video_container = av.open(self.media_path)
+        self.time_base = self.video_container.streams.video[0].time_base
+        self.average_rate = self.video_container.streams.video[0].average_rate
+        self.duration_ms = int(self.video_container.duration / 1000)
+        self.current_pos_ms = 0
 
-        self.cap = cv2.VideoCapture(self.media_path)
-        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
-        self.duration_ms = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)/self.fps * 1000
-        self.current_pos_ms = 0.0
+        # self.cap = cv2.VideoCapture(self.media_path)
+        # self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        # self.duration_ms = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)/self.fps * 1000
+        # self.current_pos_ms = 0.0
         self.is_playing = True
         self.command_queue = []
         self.last_frame = None
+
+        self.first_keyframe_ms = None
 
         # All commands return a boolean to indicate if 
         # the next frame should be sampled. For example,
@@ -47,29 +59,40 @@ class VideoDigest(MediaDigest):
         if not self.is_playing and not should_sample:
             return self.last_frame
 
-        ret, frame = self.cap.read()
-        self.last_frame = frame
-        # get current time of frame
-        self.current_pos_ms = self.cap.get(cv2.CAP_PROP_POS_MSEC)
-        if not ret:
+        try:
+            frame = next(self.video_container.decode(video=0))
+            if self.first_keyframe_ms is None and frame.key_frame:
+                self.first_keyframe_ms = int(frame.time * 1000) # convert to milliseconds
+            self.last_frame = frame.to_rgb().to_ndarray()[:, :, ::-1] # convert to BGR
+            # get current time of frame in ms
+            self.current_pos_ms = int(frame.time * 1000)
+        except StopIteration:
+                raise EndMediaError("End of video reached")
+        except av.error.EOFError:
             raise EndMediaError("End of video reached")
+        
         return self.last_frame
     
     def _skip_frame(self) -> bool:
-        curr_frame_num = self.cap.get(cv2.CAP_PROP_POS_FRAMES)
-        if curr_frame_num + 1 < self.cap.get(cv2.CAP_PROP_FRAME_COUNT):
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, curr_frame_num + 1)
+        """ Skip to the next key frame """
+        self.video_container.seek(self.current_pos_ms * 1000, backward=False)
         return True
 
     def _prev_frame(self) -> bool:
-        pos = self.cap.get(cv2.CAP_PROP_POS_FRAMES)
-        if pos > 0:
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, pos-2)
+        """ Seek to the previous key frame """
+        # sample_time = -1
+        sample_time = (self.current_pos_ms-1000) * 1000
+        if self.first_keyframe_ms is None:
+            sample_time = -1
+        
+        try:
+            self.video_container.seek(sample_time, backward=True, any_frame=False)
+        except:
+            self.video_container.seek(-1, backward=True, any_frame=True)
         return True
-    
+
     def _rewind(self) -> bool:
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        # set playing to True
+        self.video_container.seek(-1, backward=True, any_frame=True)
         self.is_playing = True
         return True
     
@@ -79,6 +102,5 @@ class VideoDigest(MediaDigest):
     
     def close(self):
         """ Close the video capture and release the resources"""
-        if self.cap:
-            self.cap.release()
+        self.video_container.close()
         
